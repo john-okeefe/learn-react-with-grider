720122cecd7fe26ff893cc67f001966f827cb986
BuildTools <unconfigured@null.spigotmc.org>
2017-01-15 11:45:34 -0600

    Code caught up to Lesson 11.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component. This component should produce some HTML
 const App = () => {
   return <div>Hi!</div>;
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
+//<App /> instantiates (Calls) the variable in JSX format.
+//.render requires a second argument to say where in the HTML document we are putting the function.
+ReactDOM.render(<App />, document.querySelector('.container'));
